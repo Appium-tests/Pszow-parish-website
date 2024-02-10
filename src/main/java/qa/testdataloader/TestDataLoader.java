6751f1304ce6289e6efea93512ba80865cdbf765
pawelaksman <pablo_xmen@op.pl>
2024-02-10 13:21:29 +0100

    Added the filed

--- src/main/java/qa/testdataloader/TestDataLoader.java
+++ src/main/java/qa/testdataloader/TestDataLoader.java
@@ -1,5 +1,6 @@
 package qa.testdataloader;
 
+import lombok.Getter;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -10,14 +11,16 @@ import java.io.Reader;
 public class TestDataLoader {
 
     private static final String PATH = "./src/test/resources/testdata/";
+    @Getter
+    private static String source;
 
-    public static String fetch(String filename) {
+    public static void fetch(String filename) {
 
         try {
             Reader reader = new FileReader(PATH + filename + ".json");
             JSONParser jsonParser = new JSONParser();
             Object object = jsonParser.parse(reader);
-            return object.toString();
+            source = object.toString();
         } catch (IOException | ParseException e) {
             throw new RuntimeException(e);
         }

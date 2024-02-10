eb31dca96812e52c9985adcc0f8d2d1084b17f68
pawelaksman <pablo_xmen@op.pl>
2024-02-10 13:23:16 +0100

    Added methods

--- src/main/java/qa/listener/TestListener.java
+++ src/main/java/qa/listener/TestListener.java
@@ -1,9 +1,23 @@
 package qa.listener;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
+
+
+    @Override
+    public void onStart(ITestContext context) {
+
+
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+
+
+    }
 
     @Override
     public void onTestStart(ITestResult result) {
@@ -11,8 +25,15 @@ public class TestListener implements ITestListener {
     }
 
     @Override
-    public void onFinish(ITestResult result) {
+    public void onTestSuccess(ITestResult result) {
+    }
 
+    @Override
+    public void onTestFailure(ITestResult result) {
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
 
     }
 }

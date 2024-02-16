da21401520c4dbf4814148ab60757fb4739ad0ae
pawelaksman <pablo_xmen@op.pl>
2024-02-16 15:39:28 +0100

    Added the Logger

--- src/main/java/qa/listener/TestListener.java
+++ src/main/java/qa/listener/TestListener.java
@@ -1,39 +1,36 @@
 package qa.listener;
 
-import org.testng.ITestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
 
+    private Logger logger = LoggerFactory.getLogger(TestListener.class);
 
-    @Override
-    public void onStart(ITestContext context) {
-
-
-    }
-
-    @Override
-    public void onFinish(ITestContext context) {
-
-
-    }
 
     @Override
     public void onTestStart(ITestResult result) {
 
+        logger.info("Test start on " + result.getMethod());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
+
+        logger.info("PASSED on " + result.getMethod());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
+
+        logger.info("FAILED on " + result.getThrowable().toString());
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
 
+        logger.info("SKIPPED on " + result.getMethod());
     }
 }

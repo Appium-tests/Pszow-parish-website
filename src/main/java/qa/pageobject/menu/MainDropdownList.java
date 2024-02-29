feee064050fb0295c414a7653c7d0cc3c20dee0c
pawelaksman <pablo_xmen@op.pl>
2024-02-29 15:46:56 +0100

    Added methods

--- src/main/java/qa/pageobject/menu/MainDropdownList.java
+++ src/main/java/qa/pageobject/menu/MainDropdownList.java
@@ -18,4 +18,60 @@ public class MainDropdownList extends DropdownList {
 
         return closeButton;
     }
+
+    @FindBy(xpath = "//li[@id='menu-item-24730']/a")
+    WebElement announcementsLink;
+
+    @FindBy(xpath = "//li[@id='menu-item-24731']/a")
+    WebElement intentionsLink;
+
+    @FindBy(xpath = "//li[@id='menu-item-24732']/a")
+    WebElement funeralsLink;
+
+    @FindBy(xpath = "//li[@id=\"menu-item-24733\"]/a")
+    WebElement stewardsLink;
+
+    @FindBy(xpath = "//li[@id='menu-item-24734']/a")
+    WebElement priestsLink;
+
+    @FindBy(xpath = "//li[@id='menu-item-24736']/a")
+    WebElement confessionLink;
+
+    @FindBy(xpath = "//li[@id='menu-item-24702']/a")
+    WebElement officeLink;
+
+    public void touchAnnouncementsLink() {
+
+        touchLink(announcementsLink);
+    }
+
+    public void touchIntentionsLink() {
+
+        touchLink(intentionsLink);
+    }
+
+    public void touchFuneralsLink() {
+
+        touchLink(funeralsLink);
+    }
+
+    public void touchStewardsLink() {
+
+        touchLink(stewardsLink);
+    }
+
+    public void touchPriestsLink() {
+
+        touchLink(priestsLink);
+    }
+
+    public void touchConfessionLink() {
+
+        touchLink(confessionLink);
+    }
+
+    public void touchOfficeLink() {
+
+        touchLink(officeLink);
+    }
 }

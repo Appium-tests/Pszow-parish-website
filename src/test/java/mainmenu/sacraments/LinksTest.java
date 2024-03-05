1729834570d630372669d78f19ca77acf97f0f79
pawelaksman <pablo_xmen@op.pl>
2024-03-06 00:34:32 +0100

    Added methods

--- src/test/java/mainmenu/sacraments/LinksTest.java
+++ src/test/java/mainmenu/sacraments/LinksTest.java
@@ -1,42 +1,70 @@
 package mainmenu.sacraments;
 
-import io.qameta.allure.Allure;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
+import base.BaseTest;
+import io.qameta.allure.*;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import base.DropdownListTest;
-import qa.dataprovidernames.DataProviderNames;
-import qa.dataproviders.DropdownListDataProviders;
 import qa.enums.URLs;
-import qa.models.LinkData;
 import qa.pageobject.menu.SacramentsDropdownList;
-import qa.steps.DropdownListSteps;
 
 @Epic("E2E")
 @Feature("The \"Sakramenty\" dropdown list links")
-public class LinksTest extends DropdownListTest {
+public class LinksTest extends BaseTest {
 
-    DropdownListSteps steps;
+    private SacramentsDropdownList sacramentsDropdownList;
 
     @BeforeMethod
     public void init() {
 
         goToUrl(URLs.HOME_PAGE.getName());
         expandMainDropdownList();
-        steps = new DropdownListSteps(new SacramentsDropdownList(getDriver()));
-        steps.touchTriggerElement();
+        sacramentsDropdownList = new SacramentsDropdownList(getDriver());
+        sacramentsDropdownList.touchTriggerElement();
     }
 
-    @Test(dataProvider = DataProviderNames.SACRAMENTS, dataProviderClass = DropdownListDataProviders.class)
-    @QaseId(10)
-    @QaseTitle("The \"Sakramenty Święte\" dropdown list links")
-    public void links(LinkData linkData) {
+    @Test(priority = 1)
+    @Severity(SeverityLevel.CRITICAL)
+    @QaseId(24)
+    @QaseTitle("The \"Chrzest\" link")
+    @Description("The \"Chrzest\" link")
+    public void christeningLink() {
 
-        Allure.description("Touching the " + linkData.getValue() + " link");
-        steps.touchLink(linkData.getValue());
-        waitForUrl(linkData.getUrl(), "Incorrect url");
+        sacramentsDropdownList.touchChristeningLink();
+        waitForUrl(URLs.CHRISTENING_PAGE.getName(), "The page \"" + URLs.CHRISTENING_PAGE.getName() + "\" has not been opened");
+    }
+
+    @Test(priority = 2)
+    @Severity(SeverityLevel.NORMAL)
+    @QaseId(25)
+    @QaseTitle("The \"Pierwsza Komunia\" link")
+    @Description("The \"Pierwsza Komunia\" link")
+    public void firstCommunionLink() {
+
+        sacramentsDropdownList.touchFirstCommunionLink();
+        waitForUrl(URLs.FIRST_COMMUNION_PAGE.getName(), "The page \"" + URLs.FIRST_COMMUNION_PAGE.getName() + "\" has not been opened");
+    }
+
+    @Test(priority = 4)
+    @Severity(SeverityLevel.NORMAL)
+    @QaseId(26)
+    @QaseTitle("The \"Bierzmowanie\" link")
+    @Description("The \"Bierzmowanie\" link")
+    public void confirmationLink() {
+
+        sacramentsDropdownList.touchConfirmationLink();
+        waitForUrl(URLs.CONFIRMATION_PAGE.getName(), "The page \"" + URLs.CONFIRMATION_PAGE.getName() + "\" has not been opened");
+    }
+
+    @Test(priority = 3)
+    @Severity(SeverityLevel.NORMAL)
+    @QaseId(24)
+    @QaseTitle("The \"Małżeństwo\" link")
+    @Description("The \"Małżeństwo\" link")
+    public void marriageLink() {
+
+        sacramentsDropdownList.touchMarriageLink();
+        waitForUrl(URLs.MARRIAGE_PAGE.getName(), "The page \"" + URLs.MARRIAGE_PAGE.getName() + "\" has not been opened");
     }
 }

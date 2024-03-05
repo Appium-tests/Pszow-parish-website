8b13a6b99543ffb31c1a06bd00f4fa7673a92d83
pawelaksman <pablo_xmen@op.pl>
2024-03-06 00:48:14 +0100

    Changed the inheritance

--- src/test/java/mainmenu/worthseeing/ContentsTest.java
+++ src/test/java/mainmenu/worthseeing/ContentsTest.java
@@ -1,5 +1,6 @@
 package mainmenu.worthseeing;
 
+import base.BaseTest;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
@@ -7,42 +8,40 @@ import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import base.DropdownListTest;
 import qa.enums.URLs;
 import qa.pageobject.menu.WorthSeeingDropdownList;
-import qa.steps.DropdownListSteps;
 
 @Epic("E2E")
 @Feature("Expanding and collapsing the \"Warto zobaczyć\" dropdown list")
-public class ContentsTest extends DropdownListTest {
-    private DropdownListSteps steps;
+public class ContentsTest extends BaseTest {
+    private WorthSeeingDropdownList worthSeeingDropdownList;
 
     @BeforeMethod
     public void init() {
 
         goToUrl(URLs.HOME_PAGE.getName());
         expandMainDropdownList();
-        steps = new DropdownListSteps(new WorthSeeingDropdownList(getDriver()));
+        worthSeeingDropdownList = new WorthSeeingDropdownList(getDriver());
     }
 
     @Test(priority = 1)
-    @QaseId(14)
+    @QaseId(41)
     @QaseTitle("Expanding the \"Warto zobaczyć\" dropdown list")
     @Description("Expanding the \"Warto zobaczyć\" dropdown list")
     public void expandingDropdownList() {
 
-        steps.touchTriggerElement();
-        checkContentsVisibility(steps.getDropdownList().getContentsLocator(), "The \"Warto zobaczyć\" dropdown list is not expanded");
+        worthSeeingDropdownList.touchTriggerElement();
+        checkContentsVisibility(worthSeeingDropdownList.getContentsLocator(), "The \"Warto zobaczyć\" dropdown list is not expanded");
     }
 
     @Test(priority = 2)
-    @QaseId(15)
+    @QaseId(42)
     @QaseTitle("Collapsing the \"Warto zobaczyć\" dropdown list")
     @Description("Collapsing the \"Warto zobaczyć\" dropdown list")
     public void collapsingDropdownList() {
 
-        steps.touchTriggerElement();
-        steps.touchTriggerElement();
-        checkContentsInvisibility(steps.getDropdownList().getContentsLocator(), "The \"Warto zobaczyć\" dropdown list is not collapsed");
+        worthSeeingDropdownList.touchTriggerElement();
+        worthSeeingDropdownList.touchTriggerElement();
+        checkContentsInvisibility(worthSeeingDropdownList.getContentsLocator(), "The \"Warto zobaczyć\" dropdown list is not collapsed");
     }
 }

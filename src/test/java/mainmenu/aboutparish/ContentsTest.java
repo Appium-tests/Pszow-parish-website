467a6c05215a9b4321dc873c07a57278f2f523da
pawelaksman <pablo_xmen@op.pl>
2024-03-05 23:58:47 +0100

    Added annotations

--- src/test/java/mainmenu/aboutparish/ContentsTest.java
+++ src/test/java/mainmenu/aboutparish/ContentsTest.java
@@ -1,8 +1,6 @@
 package mainmenu.aboutparish;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
+import io.qameta.allure.*;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
 import org.testng.annotations.BeforeMethod;
@@ -10,39 +8,40 @@ import org.testng.annotations.Test;
 import base.DropdownListTest;
 import qa.enums.URLs;
 import qa.pageobject.menu.AboutParishDropdownList;
-import qa.steps.DropdownListSteps;
 
 @Epic("E2E")
 @Feature("Expanding and collapsing the \"O parafii\" dropdown list")
 public class ContentsTest extends DropdownListTest {
-    private DropdownListSteps steps;
+    private AboutParishDropdownList aboutParishDropdownList;
 
     @BeforeMethod
     public void init() {
 
         goToUrl(URLs.HOME_PAGE.getName());
         expandMainDropdownList();
-        steps = new DropdownListSteps(new AboutParishDropdownList(getDriver()));
+        aboutParishDropdownList = new AboutParishDropdownList(getDriver());
     }
 
     @Test(priority = 1)
-    @QaseId(5)
+    @Severity(SeverityLevel.CRITICAL)
+    @QaseId(11)
     @QaseTitle("Expanding the \"O Parafii\" drop-down list")
     @Description("Expanding the menu dropdown list")
     public void expandingDropdownList() {
 
-        steps.touchTriggerElement();
-        checkContentsVisibility(steps.getDropdownList().getContentsLocator(), "The \"O Parafii\" dropdown list is not expanded");
+        aboutParishDropdownList.touchTriggerElement();
+        checkContentsVisibility(aboutParishDropdownList.getContentsLocator(), "The \"O Parafii\" dropdown list is not expanded");
     }
 
     @Test(priority = 2)
-    @QaseId(6)
+    @Severity(SeverityLevel.CRITICAL)
+    @QaseId(12)
     @QaseTitle("Collapsing the \"O Parafii\" drop-down list")
     @Description("Collapsing the menu dropdown list")
     public void collapsingDropdownList() {
 
-        steps.touchTriggerElement();
-        steps.touchTriggerElement();
-        checkContentsInvisibility(steps.getDropdownList().getContentsLocator(), "The \"O Parafii\" dropdown list is not collapsed");
+        aboutParishDropdownList.touchTriggerElement();
+        aboutParishDropdownList.touchTriggerElement();
+        checkContentsInvisibility(aboutParishDropdownList.getContentsLocator(), "The \"O Parafii\" dropdown list is not collapsed");
     }
 }

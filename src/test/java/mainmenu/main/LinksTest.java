b3554e67db373f4da929102c18521208631e8963
pawelaksman <pablo_xmen@op.pl>
2024-03-05 23:21:16 +0100

    Added priorities

--- src/test/java/mainmenu/main/LinksTest.java
+++ src/test/java/mainmenu/main/LinksTest.java
@@ -1,104 +1,101 @@
 package mainmenu.main;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
+import base.BaseTest;
+import io.qameta.allure.*;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import base.DropdownListTest;
 import qa.enums.URLs;
 import qa.pageobject.menu.MainDropdownList;
 
 @Epic("E2E")
 @Feature("Main dropdown list links")
-public class LinksTest extends DropdownListTest {
+public class LinksTest extends BaseTest {
     private MainDropdownList mainDropdownList;
 
     @BeforeMethod
     public void init() {
 
         goToUrl(URLs.HOME_PAGE.getName());
-        expandMainDropdownList();
         mainDropdownList = new MainDropdownList(getDriver());
+        mainDropdownList.touchTriggerElement();
     }
 
-    @Test
-    @Parameters("announcementsURL")
+    @Test(priority = 1)
+    @Severity(SeverityLevel.CRITICAL)
     @QaseId(4)
     @QaseTitle("The \"Ogłoszenia duszpasterskie\" link")
     @Description("The \"Ogłoszenia duszpasterskie\" link")
-    public void announcementsLink(String announcementsURL) {
+    public void announcementsLink() {
 
         mainDropdownList.touchAnnouncementsLink();
-        waitForUrl(announcementsURL, "Incorrect url");
+        waitForUrl(URLs.ANNOUNCEMENTS_PAGE.getName(), "Incorrect url");
     }
 
-    @Test
-    @Parameters("intentionsURL")
+    @Test(priority = 2)
+    @Severity(SeverityLevel.CRITICAL)
     @QaseId(5)
     @QaseTitle("The \"Intencje mszalne\" link")
     @Description("The \"Intencje mszalne\" link")
-    public void intentionsLink(String intentionsURL) {
+    public void intentionsLink() {
 
         mainDropdownList.touchIntentionsLink();
-        waitForUrl(intentionsURL, "Incorrect url");
+        waitForUrl(URLs.INTENTION_PAGE.getName(), "Incorrect url");
     }
 
-    @Test
-    @Parameters("funeralsURL")
+    @Test(priority = 3)
+    @Severity(SeverityLevel.CRITICAL)
     @QaseId(6)
     @QaseTitle("The \"Pogrzeby\" link")
     @Description("The \"Pogrzeby\" link")
-    public void funeralsLink(String funeralsURL) {
+    public void funeralsLink() {
 
         mainDropdownList.touchFuneralsLink();
-        waitForUrl(funeralsURL, "Incorrect url");
+        waitForUrl(URLs.FUNERALS_PAGE.getName(), "Incorrect url");
     }
 
-    @Test
-    @Parameters("stewardsURL")
+    @Test(priority = 7)
+    @Severity(SeverityLevel.NORMAL)
     @QaseId(7)
     @QaseTitle("The \"Szafarze\" link")
     @Description("The \"Szafarze\" link")
-    public void stewardsLink(String stewardsURL) {
+    public void stewardsLink() {
 
         mainDropdownList.touchStewardsLink();
-        waitForUrl(stewardsURL, "Incorrect url");
+        waitForUrl(URLs.STEWARDS_PAGE.getName(), "Incorrect url");
     }
 
-    @Test
-    @Parameters("priestsURL")
+    @Test(priority = 6)
+    @Severity(SeverityLevel.NORMAL)
     @QaseId(8)
     @QaseTitle("The \"Duszpasterze\" link")
     @Description("The \"Duszpasterze\" link")
-    public void priestsLink(String priestsURL) {
+    public void priestsLink() {
 
         mainDropdownList.touchPriestsLink();
-        waitForUrl(priestsURL, "Incorrect url");
+        waitForUrl(URLs.PRIESTS_PAGE.getName(), "Incorrect url");
     }
 
-    @Test
-    @Parameters("confessionURL")
+    @Test(priority = 5)
+    @Severity(SeverityLevel.CRITICAL)
     @QaseId(9)
     @QaseTitle("The \"Spowiedź\" link")
     @Description("The \"Spowiedź\" link")
-    public void confessionLink(String confessionURL) {
+    public void confessionLink() {
 
         mainDropdownList.touchConfessionLink();
-        waitForUrl(confessionURL, "Incorrect url");
+        waitForUrl(URLs.CONFESSION_PAGE.getName(), "Incorrect url");
     }
 
-    @Test
-    @Parameters("officeURL")
+    @Test(priority = 4)
+    @Severity(SeverityLevel.CRITICAL)
     @QaseId(10)
     @QaseTitle("The \"Kancelaria\" link")
     @Description("The \"Kancelaria\" link")
-    public void officeLink(String officeURL) {
+    public void officeLink() {
 
         mainDropdownList.touchOfficeLink();
-        waitForUrl(officeURL, "Incorrect url");
+        waitForUrl(URLs.OFFICE_PAGE.getName(), "Incorrect url");
     }
 }

782a1964ecc700fe6fc881c0773e95a9b6d66bfd
pawelaksman <pablo_xmen@op.pl>
2024-03-06 00:19:40 +0100

    Changed the inheritance

--- src/test/java/mainmenu/priesthood/ContentsTest.java
+++ src/test/java/mainmenu/priesthood/ContentsTest.java
@@ -1,48 +1,46 @@
 package mainmenu.priesthood;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
+import base.BaseTest;
+import io.qameta.allure.*;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import base.DropdownListTest;
 import qa.enums.URLs;
 import qa.pageobject.menu.PriesthoodDropdownList;
-import qa.steps.DropdownListSteps;
 
 @Epic("E2E")
 @Feature("Expanding and collapsing the \"Duszpasterstwo\" dropdown list")
-public class ContentsTest extends DropdownListTest {
-    private DropdownListSteps steps;
+public class ContentsTest extends BaseTest {
+    private PriesthoodDropdownList priesthoodDropdownList;
 
     @BeforeMethod
     public void init() {
 
         goToUrl(URLs.HOME_PAGE.getName());
         expandMainDropdownList();
-        steps = new DropdownListSteps(new PriesthoodDropdownList(getDriver()));
+        priesthoodDropdownList = new PriesthoodDropdownList(getDriver());
     }
 
     @Test(priority = 1)
-    @QaseId(11)
+    @Severity(SeverityLevel.CRITICAL)
+    @QaseId(28)
     @QaseTitle("Expanding the \"Duszpasterstwo\" dropdown list")
     @Description("Expanding the \"Duszpasterstwo\" dropdown list")
     public void expandingDropdownList() {
 
-        steps.touchTriggerElement();
-        checkContentsVisibility(steps.getDropdownList().getContentsLocator(), "The \"Duszpasterstwo\" dropdown list is not expanded");
+        priesthoodDropdownList.touchTriggerElement();
+        checkContentsVisibility(priesthoodDropdownList.getContentsLocator(), "The \"Duszpasterstwo\" dropdown list is not expanded");
     }
 
     @Test(priority = 2)
-    @QaseId(12)
+    @QaseId(29)
     @QaseTitle("Collapsing the \"Duszpasterstwo\" dropdown list")
     @Description("Collapsing the \"Duszpasterstwo\" dropdown list")
     public void collapsingDropdownList() {
 
-        steps.touchTriggerElement();
-        steps.touchTriggerElement();
-        checkContentsInvisibility(steps.getDropdownList().getContentsLocator(), "The \"Duszpasterstwo\" dropdown list is not collapsed");
+        priesthoodDropdownList.touchTriggerElement();
+        priesthoodDropdownList.touchTriggerElement();
+        checkContentsInvisibility(priesthoodDropdownList.getContentsLocator(), "The \"Duszpasterstwo\" dropdown list is not collapsed");
     }
 }

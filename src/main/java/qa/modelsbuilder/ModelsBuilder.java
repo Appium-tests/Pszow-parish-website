6194946a6a0a6b5005306fe0839e0eedef6830f4
pawelaksman <pablo_xmen@op.pl>
2024-03-06 00:48:59 +0100

    Removed the method

--- src/main/java/qa/modelsbuilder/ModelsBuilder.java
+++ src/main/java/qa/modelsbuilder/ModelsBuilder.java
@@ -2,26 +2,11 @@ package qa.modelsbuilder;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import qa.models.LinkData;
 import qa.testdataloader.TestDataLoader;
 
 import java.util.stream.IntStream;
 
 public class ModelsBuilder {
-
-    public static LinkData[] getLinksData(String key) {
-
-        JSONObject jsonObject = new JSONObject(TestDataLoader.getSource());
-        JSONArray jsonArray = jsonObject.getJSONArray(key);
-
-        return IntStream.range(0, jsonArray.length())
-                .mapToObj(i -> new LinkData(
-
-                        jsonArray.getJSONObject(i).getString("value"),
-                        jsonArray.getJSONObject(i).getString("url")
-                ))
-                .toArray(LinkData[]::new);
-    }
 
     public static String[] getStrings(String key) {
 

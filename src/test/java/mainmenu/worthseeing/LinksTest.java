e85e3f486735e041debadecbd387d60b637cd2ff
pawelaksman <pablo_xmen@op.pl>
2024-03-06 00:48:35 +0100

    Added methods

--- src/test/java/mainmenu/worthseeing/LinksTest.java
+++ src/test/java/mainmenu/worthseeing/LinksTest.java
@@ -1,42 +1,81 @@
 package mainmenu.worthseeing;
 
-import io.qameta.allure.Allure;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
+import base.BaseTest;
+import io.qameta.allure.*;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import base.DropdownListTest;
-import qa.dataprovidernames.DataProviderNames;
-import qa.dataproviders.DropdownListDataProviders;
 import qa.enums.URLs;
-import qa.models.LinkData;
 import qa.pageobject.menu.WorthSeeingDropdownList;
-import qa.steps.DropdownListSteps;
 
 @Epic("E2E")
 @Feature("The \"Warto zobaczyć\" dropdown list links")
-public class LinksTest extends DropdownListTest {
+public class LinksTest extends BaseTest {
 
-    DropdownListSteps steps;
+    private WorthSeeingDropdownList worthSeeingDropdownList;
 
     @BeforeMethod
     public void init() {
 
         goToUrl(URLs.HOME_PAGE.getName());
         expandMainDropdownList();
-        steps = new DropdownListSteps(new WorthSeeingDropdownList(getDriver()));
-        steps.touchTriggerElement();
+        worthSeeingDropdownList = new WorthSeeingDropdownList(getDriver());
+        worthSeeingDropdownList.touchTriggerElement();
     }
 
-    @Test(dataProvider = DataProviderNames.WORTH_SEEING, dataProviderClass = DropdownListDataProviders.class)
-    @QaseId(16)
-    @QaseTitle("The \"Warto zobaczyć\" dropdown list links")
-    public void links(LinkData linkData) {
+    @Test(priority = 4)
+    @Severity(SeverityLevel.NORMAL)
+    @QaseId(43)
+    @QaseTitle("The \"Litania do Pani Usmiechniętej\" link")
+    @Description("The \"Litania do Pani Uśmiechniętej\" link")
+    public void litanyLink() {
 
-        Allure.description("Touching the " + linkData.getValue() + " link");
-        steps.touchLink(linkData.getValue());
-        waitForUrl(linkData.getUrl(), "Incorrect url");
+        worthSeeingDropdownList.touchLitanyLink();
+        waitForUrl(URLs.LITANY_PAGE.getName(), "The page \"" + URLs.LITANY_PAGE.getName() + "\" has not been opened");
+    }
+
+    @Test(priority = 1)
+    @Severity(SeverityLevel.NORMAL)
+    @QaseId(44)
+    @QaseTitle("The \"Regulamin cmentarza\" link")
+    @Description("The \"Regulamin cmentarza\" link")
+    public void cementeryRegulationLink() {
+
+        worthSeeingDropdownList.touchCementeryRegulationLink();
+        waitForUrl(URLs.CEMENTERY_REGULATION.getName(), "The page \"" + URLs.CEMENTERY_REGULATION.getName() + "\" has not been opened");
+    }
+
+    @Test(priority = 5)
+    @Severity(SeverityLevel.MINOR)
+    @QaseId(45)
+    @QaseTitle("The \"Delegat ds. ochrony dzieci i młodzieży\" link")
+    @Description("The \"Delegat ds. ochrony dzieci i młodzieży\" link")
+    public void delegateLink() {
+
+        worthSeeingDropdownList.touchDelegateLink();
+        waitForUrl(URLs.DELEGATE_PAGE.getName(), "The page \"" + URLs.DELEGATE_PAGE.getName() + "\" has not been opened");
+    }
+
+    @Test(priority = 2)
+    @Severity(SeverityLevel.CRITICAL)
+    @QaseId(46)
+    @QaseTitle("The \"Archidiecezja Katowicka\" link")
+    @Description("The \"Archidiecezja Katowicka\" link")
+    public void archdioceseLink() {
+
+        worthSeeingDropdownList.touchArchdioceseLink();
+        waitForUrl(URLs.ARCHDIOCESE.getName(), "The page \"" + URLs.ARCHDIOCESE.getName() + "\" has not been opened");
+    }
+
+    @Test(priority = 3)
+    @Severity(SeverityLevel.CRITICAL)
+    @QaseId(47)
+    @QaseTitle("The \"Czytania liturgiczne\" link")
+    @Description("The \"Czytania liturgiczne\" link")
+    public void readingLink() {
+
+        worthSeeingDropdownList.touchReadingLink();
+        waitForUrl(URLs.READING.getName(), "The page \"" + URLs.READING.getName() + "\" has not been opened");
     }
 }

1eb664fbc3eb0cdace3cb79f6409db8ada77f06f
pawelaksman <pablo_xmen@op.pl>
2024-03-06 15:21:03 +0100

    Removed method

--- src/test/java/photogallery/PhotoGalleryPageTest.java
+++ src/test/java/photogallery/PhotoGalleryPageTest.java
@@ -1,73 +1,43 @@
 package photogallery;
 
-import io.qameta.allure.Allure;
+import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 import base.BaseTest;
+import qa.dataprovidernames.DataProviderNames;
+import qa.dataproviders.DataProviders;
 import qa.enums.URLs;
-import qa.pageobject.CookiesMessageBox;
-import qa.steps.PhotoGalleryPageSteps;
+import qa.models.Item;
+import qa.pageobject.photogallery.PhotoGalleryPage;
 
 @Epic("E2E")
 @Feature("Expanding and collapsing the photo gallery items")
 public class PhotoGalleryPageTest extends BaseTest {
-    private PhotoGalleryPageSteps steps;
+
+    private PhotoGalleryPage photoGalleryPage;
 
     @BeforeMethod
     public void init() throws IllegalAccessException {
 
         goToUrl(URLs.PHOTO_GALLERY_PAGE.getName());
-
-        CookiesMessageBox cookiesMessageBox = new CookiesMessageBox(getDriver());
-        cookiesMessageBox.tapAcceptButton();
-        steps = new PhotoGalleryPageSteps(getDriver());
-        steps.getPhotoGalleryPage().findItems();
+        photoGalleryPage = new PhotoGalleryPage(getDriver());
     }
 
 
-    @Test
-    @QaseId(18)
-    @QaseTitle("Expanding photo gallery contents")
-    public void expandingGallery() {
+    @Test(dataProvider = DataProviderNames.ITEMS, dataProviderClass = DataProviders.class)
+    @QaseId(54)
+    @QaseTitle("Expanding and collapsing the content of the photo gallery")
+    @Description("Expanding and collapsing the content of the photo gallery")
+    public void contentVisibility(Item item) {
 
-        SoftAssert softAssert = new SoftAssert();
-
-        for (int i = 1; i < steps.getPhotoGalleryPage().getItemsCount(); i++) {
-
-            Allure.description("Expanding the \"" + steps.getPhotoGalleryPage().getItem(i).getTitle() + "\" photo gallery");
-            getInteractions().scroll(steps.getPhotoGalleryPage().getItem(i).getIconClosedLocator());
-            steps.touchIconClosed(i);
-
-            softAssert.assertFalse(steps.getPhotoGalleryPage().getItem(i).getGalleryContentLocator().isEmpty(),
-                    "The photo gallery \"" + steps.getPhotoGalleryPage().getItem(i).getTitle() + "\" is not expanded");
-        }
-
-        softAssert.assertAll();
+        photoGalleryPage.touchTriggerElement(item.getTabTitle());
+        checkContentsVisibility(getDriver().findElement(By.id(item.getTabContent())), "The \"" + item.getTabContent() + "\" is not visible");
+        photoGalleryPage.touchTriggerElement(item.getTabTitle());
+        checkContentsInvisibility(getDriver().findElement(By.id(item.getTabContent())), "The \"" + item.getTabContent() + "\" is not hidden");
     }
-
-    @Test
-    @QaseId(19)
-    @QaseTitle("Collapsing photo gallery contents")
-    public void collapsingGallery() {
-
-        SoftAssert softAssert = new SoftAssert();
-
-        for (int i = 1; i < steps.getPhotoGalleryPage().getItemsCount(); i++) {
-
-           Allure.description("Collapsing the \"" + steps.getPhotoGalleryPage().getItem(i).getTitle() + "\" photo gallery");
-           getInteractions().scroll(steps.getPhotoGalleryPage().getItem(i).getIconClosedLocator());
-           steps.touchIconClosed(i);
-           steps.touchIconOpened(i);
-
-           softAssert.assertTrue(steps.getPhotoGalleryPage().getItem(i).getGalleryContentLocator().isEmpty(),
-                    "The photo gallery \"" + steps.getPhotoGalleryPage().getItem(i).getTitle() + "\" is not collapsed");
-        }
-
-        softAssert.assertAll();
-   }
 }

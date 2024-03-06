806ceaa0db3483f8cc9f36fe0cdc19b577e6ba2f
pawelaksman <pablo_xmen@op.pl>
2024-03-06 15:21:39 +0100

    Added locators and methods

--- src/main/java/qa/pageobject/socialmedia/SocialMedia.java
+++ src/main/java/qa/pageobject/socialmedia/SocialMedia.java
@@ -1,7 +1,8 @@
 package qa.pageobject.socialmedia;
 
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.base.BasePage;
 
@@ -12,8 +13,28 @@ public class SocialMedia extends BasePage {
         super(driver);
     }
 
-    public void touchIcon(String selector) {
+    public void touchIcon(WebElement element) {
 
-        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(By.xpath(selector))).click();
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    @FindBy(xpath = "//div[@id='mobile-dropdown']/nav/div[2]/div/ul/li[1]/a")
+    WebElement facebookLink;
+
+    @FindBy(xpath = "//div[@id='mobile-dropdown']/nav/div[2]/div/ul/li[2]/a")
+    WebElement youtubeLink;
+
+    @io.qameta.allure.Step("Touch the Facebook link")
+    @io.qase.api.annotation.Step("Touch the Facebook link")
+    public void touchFacebookIcon() {
+
+        touchIcon(facebookLink);
+    }
+
+    @io.qameta.allure.Step("Touch the Facebook link")
+    @io.qase.api.annotation.Step("Touch the Facebook link")
+    public void touchYoutubeIcon() {
+
+        touchIcon(youtubeLink);
     }
 }

07f28d3a1a8a75c9bc68bbe065f2b42cc9656b06
pawelaksman <pablo_xmen@op.pl>
2024-03-06 15:19:08 +0100

    Removed fields

--- src/main/java/qa/dataprovidernames/DataProviderNames.java
+++ src/main/java/qa/dataprovidernames/DataProviderNames.java
@@ -2,12 +2,7 @@ package qa.dataprovidernames;
 
 public class DataProviderNames {
 
-    public static final String MAIN_DROPDOWN_LIST = "main";
-    public static final String ABOUT_PARISH = "aboutParish";
-    public static final String SACRAMENTS = "sacraments";
-    public static final String PRIESTHOOD = "priesthood";
-    public static final String WORTH_SEEING = "worthSeeing";
-    public static final String SOCIAL_MEDIA = "socialMedia";
+    public static final String ITEMS = "items";
     public static final String CORRECT = "correct";
     public static final String INCORRECT = "incorrect";
     public static final String NAUGHTY_STRINGS = "naughtyStrings";
